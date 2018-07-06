88fc0f35471e8b5c47da2a213f70f14caed69ed1
w0nd3r0us <steven.grewal@gmail.com>
2018-07-06 14:11:08 -0400

    commit with first round of changes

--- ex1.rb
+++ ex1.rb
@@ -45,12 +45,13 @@ def check_syntax(str)
   return open.empty?
 end
 
-puts check_syntax("(this)[] is some text")
+puts check_syntax("<html> (this)[] is some text</html>")
 puts "true"
-puts check_syntax("(this)] is some text")
+puts check_syntax("<html> (this)] is some text</html>")
 puts "* You have a syntax error: there is an unexpected ] where there is nothing to close."
-puts check_syntax("[(this] is some text")
+puts check_syntax("<html> [(this] is some text</html>")
 puts "* You have a syntax error: there is an unexpected ] where there should be a )"
-puts check_syntax("[this][ is some text")
+puts check_syntax("<html> [this][ is some text</html>")
 puts "* You have a syntax error: the string ended without a closing ]"
-puts check_syntax("[this] is some text")
+puts check_syntax("<html> [this] is some text</html>")
+puts "* You have a syntax error: the string ended without a closing >"
